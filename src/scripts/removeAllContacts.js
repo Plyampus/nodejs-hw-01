880b09f9ac85dbb4d156d043c8705d2d10cb0cf7
pavlo <pavlohvozdetskyi@protonmail.com>
2024-07-18 21:56:45 +0300

    Update Mentor Request Two

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -5,8 +5,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-const PATH_DB = path.resolve(__dirname, '../constants/contacts.json');
+const PATH_DB = path.resolve(__dirname, '../db/db.json');
 
 export const removeAllContacts = async () => {
     await fs.writeFile(PATH_DB, JSON.stringify([]));
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -2,12 +2,10 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-// Створюємо еквівалент __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Використовуємо path.resolve для створення абсолютного шляху
-const PATH_DB = path.resolve(__dirname, '../constants/contacts.json');
+const PATH_DB = path.resolve(__dirname, '../db/db.json');
 
 export const removeLastContact = async () => {
     const contacts = JSON.parse(await fs.readFile(PATH_DB, 'utf8'));
